0359b4d9b2bc6915e3912f54e0975ce7734eff6f
n1988 <n1988@LAPTOP-SBD3L46R>
2022-09-07 13:13:43 +0900

    test commit2

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 20L;
+		double double1 = 20.85;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float float1 = 355.4F;
+		Integer int1 = 500;
+		Character char1 = 'あ';
+		System.out.println(float1);
+		System.out.println(int1);
+		System.out.println(char1);
+		
+		int array1[] = new int[] {10, 20, 30, 40, 50};
+		System.out.println(array1[3]);
+	}
+}
---
+++ JavaWcp/src/Chapter4.java
@@ -0,0 +1,13 @@
+
+public class Chapter4 {
+	public static void main(String[] args) {
+		String name = "Nick";
+		System.out.println(name);
+		
+		name = "Taro";
+		System.out.println(name);
+		
+		final int LUCKY_NUMBER = 777;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
