a7b80c06b2603322d4dccacfbfc99c8e0b852441
n1988 <n1988@LAPTOP-SBD3L46R>
2022-09-07 12:54:31 +0900

    test commit

---
+++ JavaWcp/src/Calc01.java
@@ -0,0 +1,17 @@
+
+public class Calc01 {
+	public static void main(String[] args) {
+		int a;
+		int c;
+		
+		a = 5;
+		c = ++a;
+		System.out.println(a);
+		System.out.println(c);
+		
+		a = 5;
+		c = a++;
+		System.out.println(a);
+		System.out.println(c);
+	}
+}
