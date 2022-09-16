dd768f48ead279955e40e9c1fdb3af2dd29295ff
n19880405nick <n19880405nick@gmail.com>
2022-09-16 11:53:49 +0900

    java practice "kitako-1"

---
+++ JavaKitako/src/practice1.java
@@ -0,0 +1,69 @@
+
+public class practice1 {
+	public static void main(String[] args) {
+//		1-1
+		System.out.println("Hello World");
+		
+//		1-2
+		int x2 = 11;
+		System.out.println("x = " + x2);
+		
+//		1-3
+		int x3 = 13;
+		int y3 = 17;
+		System.out.println("x = " + x3 + ", y = " + y3);
+		
+//		1-4
+		int x4 = 13 + 17;
+		System.out.println(x4);
+		
+//		1-5
+		System.out.println(13 * 17);
+		
+//		1-6
+		int x6 = 7;
+		x6 *= 3;
+		System.out.println(x6);
+		x6 /= 2;
+		System.out.println(x6);
+		
+//		1-7
+		int x7 = 3;
+		int y7 = 7;
+		int z7 = x7;
+		x7 = y7;
+		y7 = z7;
+		System.out.println("x = " + x7 + ", y = " + y7);
+		
+//		1-8
+		int x8 = 19;
+		int y8 = 23;
+		int z8 = x8 * y8;
+		System.out.println(z8);
+		
+//		1-9
+		int x9 = 5;
+		System.out.println(x9 * 2);
+		System.out.println(x9 * 3);
+		System.out.println(x9 * 4);
+		
+//		1-10
+		int x10 = 10;
+		System.out.println(x10);
+		System.out.println(x10 * x10);
+		System.out.println(x10 * x10 * x10);
+		
+//		1-11
+		int x11 = 8;
+		System.out.println(x11 / 4);
+		System.out.println(x11 % 3);
+		
+//		1-12
+		int x12 = 3;
+		x12++;
+		System.out.println(x12);
+		x12--;
+		x12--;
+		System.out.println(x12);
+	}
+}
